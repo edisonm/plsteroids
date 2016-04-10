c29bb71b54e807b1c1f99a0af2462dc1ea16789d
Edison Mera <efmera@gmail.com>
2016-04-10 18:44:14 +0200

    Added ctchecks.plt to autotester.pl

--- autotester.pl
+++ autotester.pl
@@ -1,12 +1,14 @@
 :- module(autotester, [cover_tests/0]).
 
-:- [xlibrary/tests/i18n/'i18n.plt'].
-:- [xtools/tests/'assrt_meta.plt'].
 :- [assertions/tests/'assertions.plt'].
 :- [assertions/tests/foreign/'foreign.plt'].
 :- [refactor/tests/'gcb.plt'].
-:- [rtchecks/tests/'rtchecks.plt'].
 :- [refactor/tests/'refactor.plt'].
+:- [rtchecks/tests/'rtchecks.plt'].
+:- [xlibrary/tests/i18n/'i18n_2.plt'].
+:- [xlibrary/tests/i18n/'i18n.plt'].
+:- [xtools/tests/'assrt_meta.plt'].
+:- [xtools/tests/'ctchecks.plt'].
 :- [library(gcover)].
 :- [library(ws_cover)].
 
