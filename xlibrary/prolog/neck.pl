fa9a0e0b282bd69cb86be372b94c42dbb5b19543
Edison Mera <efmera@gmail.com>
2017-05-20 09:37:59 +0200

    git subrepo pull xlibrary
    subrepo:
      subdir:   "xlibrary"
      merged:   "712995e"
    upstream:
      origin:   "https://github.com/edisonm/xlibrary.git"
      branch:   "master"
      commit:   "712995e"
    git-subrepo:
      version:  "0.3.1"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "e37942c"

--- xlibrary/prolog/neck.pl
+++ xlibrary/prolog/neck.pl
@@ -32,53 +32,47 @@
     POSSIBILITY OF SUCH DAMAGE.
 */
 
-:- module(intercept, [intercept/3,
-                      intercept/4,
-                      send_signal/1]).
+:- module(neck, [neck/0]).
 
-:- meta_predicate
-       intercept(0,+,0),
-       intercept(0,+,+,0).
+:- use_module(library(compound_expand)).
+:- use_module(library(sequence_list)).
+:- use_module(library(list_sequence)).
 
-intercept(Goal, Signal, Bindings, Handler) :-
-    call(Goal),
-    keep_on_frame(i(Signal, Bindings, Handler)).
+%!  neck is det
+%
+%   Stablish that everything above it should be evaluated at compile time, be
+%   careful since such part should contain only predicates already defined.  In
+%   case of non-determinism, several clauses would be generated.  This is a
+%   practical way to generate automatic clauses with a proper instantiation of
+%   the head. If neck can not be expanded, it will succeed without side effects.
 
-intercept(Goal, Signal, Handler) :-
-    intercept(Goal, Signal, [], Handler).
+neck.
 
-keep_on_frame(_).
-
-send_signal(Signal) :-
-    var(Signal), !,
-    throw(error(instantiation_error, signal/1 -1)).
-send_signal(Signal) :-
-    prolog_current_frame(Frame),
-    find_parent_handler(Frame, Signal, IFrame, Handler, []),
-    call_handler(IFrame, Handler),
-    keep_on_frame(Frame).
-
-call_handler(IFrame, Handler) :-
-    call(Handler),
-    keep_on_frame(IFrame).
-
-find_parent_handler(Frame, Signal, IFrame, Handler, SFrameL) :-
-    prolog_frame_attribute(Frame, parent, Parent),
-    ( member(Parent, SFrameL)
-    ->find_parent_handler(Parent, Signal, IFrame, Handler, SFrameL)
-    ; prolog_frame_attribute(Frame, goal, Goal),
-      ( Goal \= call_handler(_, _)
-      ->( Goal \= intercept(_, Signal, _, _)
-        ->find_parent_handler(Parent, Signal, IFrame, Handler, SFrameL)
-        ; Goal = intercept(_, _, Bindings, _),
-          copy_term(Goal, intercept(_, Signal, Bindings, Handler)),
-          IFrame = Parent
-        )
-      ; Goal = call_handler(SkipFrame, _),
-        find_parent_handler(Parent, Signal, IFrame, Handler,
-                            [SkipFrame|SFrameL])
-      )
+term_expansion((Head :- Body1), ClauseL) :-
+    sequence_list(Body1, List, []),
+    append(Left, [neck|Right], List),
+    '$current_source_module'(M),
+    list_sequence(Left, Static),
+    once(( append(LRight, RRight, Right),
+           \+ ( sub_term(Lit, RRight),
+                nonvar(Lit),
+                Lit == !
+              ) % We can not move the part above a cut to a separate clause
+         )),
+    ( RRight = [_, _|_]
+    ->term_variables(t(Head, Static, LRight), VarHU),
+      sort(VarHU, VarHL),
+      term_variables(RRight, VarBU),
+      sort(VarBU, VarBL),
+      ord_intersection(VarHL, VarBL, ArgNB),
+      functor(Head, F, A),
+      variant_sha1(ArgNB-RRight, Hash),
+      format(atom(FNB), '__aux_neck_~w/~d_~w', [F, A, Hash]),
+      SepHead =.. [FNB|ArgNB],
+      list_sequence(RRight, SepBody),
+      '$expand':compile_auxiliary_clause(M, (SepHead :- SepBody)),
+      append(LRight, [SepHead], NeckL),
+      list_sequence(NeckL, NeckBody)
+    ; list_sequence(Right, NeckBody)
     ),
-    keep_on_frame(Parent), !.
-find_parent_handler(_, Signal, _, _, _) :-
-    throw(error(unintercepted_signal(Signal), signal/1 -1)).
+    findall((Head :- NeckBody), M:Static, ClauseL).
