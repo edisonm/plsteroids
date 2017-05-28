39bc0523a0c3877f615e53d21f14035b4aa3c80c
Edison Mera <efmera@gmail.com>
2017-05-28 20:05:19 +0200

    Added detection of uncovered lines

--- emacs/emacs.el
+++ emacs/emacs.el
@@ -143,4 +143,4 @@
   )
 
 (set-foreground-color "white")
-(set-default-font "-adobe-courier-bold-r-normal--25-*-100-100-m-150-iso8859-1")
+(set-default-font "-adobe-courier-medium-r-normal--20-*-100-100-m-150-iso8859-1")
