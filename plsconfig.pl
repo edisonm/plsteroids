1d07910f21337dc92478bf7efacf4f506fb2d759
Edison Mera <efmera@gmail.com>
2018-04-27 15:46:53 +0200

    target ---> target/bin

--- plsconfig.pl
+++ plsconfig.pl
@@ -5,14 +5,13 @@
 set_plroot :-
     source_location(File, _),
     directory_file_path(Dir, _, File),
-    % directory_file_path(Root, _, File),
-    directory_file_path(Dir, target, BinDir),
+    directory_file_path(Dir, 'target/bin', BinDir),
     retractall(user:file_search_path(plroot, _)),
     ( exists_directory(BinDir)
     ->assertz(user:file_search_path(plroot, BinDir)),
-      print_message(information, format("Using binary files", []))
+      print_message(informational, format("Using binary files", []))
     ; assertz(user:file_search_path(plroot, Dir)),
-      print_message(information, format("Using source files", []))
+      print_message(informational, format("Using source files", []))
     ).
 
 user:file_search_path(library, pltool(prolog)).
--- pltool.sh
+++ pltool.sh
@@ -94,8 +94,8 @@ case $1 in
     compile)
         for i in `find . -name pack.pl`; do
             pack=`basename ${i%/pack.pl}`
-            find $pack/prolog -type d -exec mkdir -p target/{} \;
-            find $pack/prolog -type f -name "*.pl" -exec ln -s ${PWD}/{} target/{} \;
+            find $pack/prolog -type d -exec mkdir -p target/bin/{} \;
+            find $pack/prolog -type f -name "*.pl" -exec ln -s ${PWD}/{} target/bin/{} \;
         done
 	swipl -q -s qcompile.pl
 	;;
