09ce747dc48e1a32d058ac3ccecc54bf7e413379
Edison Mera <efmera@gmail.com>
2018-04-28 13:29:29 +0200

    Cleaned up binary directory option: is not working yet

--- plsconfig.pl
+++ plsconfig.pl
@@ -5,14 +5,8 @@
 set_plroot :-
     source_location(File, _),
     directory_file_path(Dir, _, File),
-    directory_file_path(Dir, 'target/bin', BinDir),
     retractall(user:file_search_path(plroot, _)),
-    ( exists_directory(BinDir)
-    ->assertz(user:file_search_path(plroot, BinDir)),
-      print_message(informational, format("Using binary files", []))
-    ; assertz(user:file_search_path(plroot, Dir)),
-      print_message(informational, format("Using source files", []))
-    ).
+    assertz(user:file_search_path(plroot, Dir)).
 
 user:file_search_path(library, pltool(prolog)).
 
