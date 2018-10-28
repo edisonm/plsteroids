246d21d0131ea2f41b254480462f5040da0245cf
Edison Mera <efmera@gmail.com>
2018-10-28 10:43:33 +0100

    git subrepo clone https://github.com/edisonm/clpcd.git clpcd
    subrepo:
      subdir:   "clpcd"
      merged:   "6fa2656"
    upstream:
      origin:   "https://github.com/edisonm/clpcd.git"
      branch:   "master"
      commit:   "6fa2656"
    git-subrepo:
      version:  "0.3.1"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "a356961"

---
+++ clpcd/README.md
@@ -0,0 +1,40 @@
+clpcd
+=====
+
+Constraint logic programming over continuous domains
+
+Based on the CLP(Q,R) package by Leslie De Koninck, K.U. Leuven, available in
+SWI-Prolog.
+
+Installation
+------------
+
+To install this package, just follow the next sequence of commands in your
+SWI-Prolog shell:
+
+```bash
+  $ swipl
+  
+  ?- pack_install('https://github.com/edisonm/clpcd.git').
+  true.
+```
+
+Overview
+--------
+
+The goal of this library is to make a CLP library for continuous domains like
+reals or rationals, that is more appropriated to calculations found in
+engineering applications.
+
+Current CLP(QR) implementation work only when the system of equations are
+linear, although in some cases, the equations can be modified so that they
+become linear and make the system solvable. But also, it is not being maintained
+and contains several unfortunate bugs, failing even for very small examples. It
+also presents implementation limitations (like the linearity); probably because
+the prototype was designed some decades ago, when computational power was
+limited.
+
+This library is focused to remove those limitations, and to incorporate
+solvers for well-known non-linear equations, when such systems are identified.
+The numeric resolution of such equations will be inspired or taken form existing
+frameworks like the GNU Scientific Library (GSL) or GNU Octave.
---
+++ clpcd/prolog/clpcd/highlight.pl
@@ -0,0 +1,66 @@
+/*  Part of SWI-Prolog
+
+    Author:        Jan Wielemaker and Richard O'Keefe
+    E-mail:        J.Wielemaker@cs.vu.nl
+    WWW:           http://www.swi-prolog.org
+    Copyright (c)  2017, CWI, Amsterdam
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions
+    are met:
+
+    1. Redistributions of source code must retain the above copyright
+       notice, this list of conditions and the following disclaimer.
+
+    2. Redistributions in binary form must reproduce the above copyright
+       notice, this list of conditions and the following disclaimer in
+       the documentation and/or other materials provided with the
+       distribution.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+    "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+    LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+    FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+    COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+    INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+    BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+    LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+    CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+    LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+    ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+    POSSIBILITY OF SUCH DAMAGE.
+*/
+
+:- module(clpcd_highlight, []).
+
+:- multifile
+        clpcd_module/1,
+	prolog_colour:goal_colours/3.
+
+prolog_colour:goal_colours({Constraints}, imported(File), Colours) :-
+	clpcd_module(Module),
+	module_property(Module, file(File)), !,
+	Colours = goal(imported(File)) - ConstraintColours,
+	constraint_colours(Constraints,	Module, ConstraintColours).
+
+constraint_colours(Var, _, classify) :-
+	var(Var), !.
+constraint_colours((R1,R2), M, classify-[C1,C2]) :- !,
+	constraint_colours(R1, M, C1),
+	constraint_colours(R2, M, C2).
+constraint_colours((R1;R2), M, classify-[C1,C2]) :- !,
+	constraint_colours(R1, M, C1),
+	constraint_colours(R2, M, C2).
+constraint_colours(Term,   M, constraint(M)-[classify,classify]) :-
+	clpcd_constraint(Term), !.
+constraint_colours(_, M, type_error(clpcd_constraint(M))) :-
+        clpcd_module(M).
+
+clpcd_constraint(_ > _).
+clpcd_constraint(_ >= _).
+clpcd_constraint(_ < _).
+clpcd_constraint(_ =< _).
+clpcd_constraint(_ =:=_).
+clpcd_constraint(_ =\= _).
+clpcd_constraint(_ = _).
