669513707e683135b75854d7e6456e8510c726da
Edison Mera <efmera@gmail.com>
2020-11-14 18:28:57 +0100

    Changed title of playground package

--- playground/pack.pl
+++ playground/pack.pl
@@ -1,6 +1,6 @@
 name(playground).
 version('0.0.1').
-title('Experimental Libraries that once mature will be moved to xlibrary').
+title('Experimental Libraries that once mature will be moved to released packages').
 keywords([sandbox,playground,library]).
 author('Edison Mera Menendez', 'http://www.edisonm.com/').
 packager('Edison Mera Menendez', 'http://www.edisonm.com/').
