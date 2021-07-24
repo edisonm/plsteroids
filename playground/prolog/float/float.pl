be7b3fb062700bbe6e853293a0b7ea0bc42683c3
Edison Mera <efmera@gmail.com>
2021-07-24 12:33:36 +0200

    Removed playground(float/)) since its content has been mostly superseded by packages mpc, mpfr and clpcd

--- playground/prolog/float/float.pl
+++
@@ -1,9 +0,0 @@
-:- module(float, []).
-
-:- reexport(library(float/evaluator)).
-
-:- use_module(library(float/complexn), []).
-:- use_module(library(float/evaluator_int), []).
-:- use_module(library(float/evaluator_num), []).
-:- use_module(library(float/evaluator_floatn), []).
-:- use_module(library(float/evaluator_complexn), []).
