39036180cf794d89b8ea88cb2f74b614757d36ee
Edison Mera <efmera@gmail.com>
2021-12-26 17:24:53 +0100

    For some unknown reason, bid_eval must be compiled first, otherwise we get errors.

--- loadall.pl
+++ loadall.pl
@@ -6,6 +6,7 @@
 :- use_module(library(ssl)).
 :- use_module(library(pldoc), []).
 :- use_module(library(apply_macros), []).
+:- use_module(library(bid_eval), []).
 
 :- set_prolog_flag(autoload, false).
 use_module_ne(Lib) :- use_module(Lib, []).
