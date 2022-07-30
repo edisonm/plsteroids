a3379a2c91313e55eb99f4d70957049180a987cb
Edison Mera <efmera@gmail.com>
2022-07-30 10:14:37 +0200

    Splitted check_unused into check_unused and calls_to modules

--- stchecks/prolog/check_unused.pl
+++ stchecks/prolog/check_unused.pl
@@ -34,13 +34,6 @@
 
 :- module(check_unused, []).
 
-/*
-
-  This analyzer is based on the Mark-and-Sweep Algorithm:
-  http://www.brpreiss.com/books/opus5/html/page424.html
-
-*/
-
 :- use_module(library(apply)).
 :- use_module(library(lists)).
 :- use_module(library(option)).
@@ -51,25 +44,26 @@
 :- use_module(library(qualify_meta_goal)).
 :- use_module(library(checkable_predicate)).
 :- use_module(library(current_defined_predicate)).
-:- use_module(library(codewalk)).
 :- use_module(library(extra_location)).
 :- use_module(library(from_utils)).
 :- use_module(library(is_entry_point)).
 :- use_module(library(location_utils)).
 :- use_module(library(option_utils)).
 :- use_module(library(ungroup_keys_values)).
-:- use_module(library(compact_goal)).
 :- use_module(library(condconc)).
+:- use_module(library(calls_to)).
+
+/** <module> Check unused predicates
+
+  This analyzer is based on the Mark-and-Sweep Algorithm:
+  http://www.brpreiss.com/books/opus5/html/page424.html
+
+*/
 
 :- multifile
     prolog:message//1.
 
 :- dynamic
-    calls_to_initialization/2,
-    calls_to_assertion/4,
-    calls_to_declaration/2,
-    calls_to_clause/3,
-    calls_to_predid/4,
     marked_assertion/2,
     marked_predid/2,
     marked_clause/1,
@@ -78,53 +72,19 @@
     marked_exported/2,
     edge/5.
 
-:- public collect_unused/4.
-collect_unused(M, MGoal, Caller, From) :-
-    record_location_meta(MGoal, M, From, all_call_refs, cu_caller_hook(Caller)).
-
 checker:check(unused, Result, Options) :-
-    check_unused(Options, Result).
-
-check_unused(Options1, Pairs) :-
-    foldl(select_option_default,
-          [method(Method1)-clause],
-          Options1, Options2),
-    ( \+ memberchk(Method1, [prolog, clause]) % only these methods are supported
-    ->Method = clause,
-      print_message(
-          warning,
-          format("Method `~w' not supported yet, using `~w' instead",
-                 [Method1, Method]))
-    ; Method = Method1
-    ),
-    option(module(M), Options1, M),
-    merge_options(Options2,
-                  [source(false), % False, otherwise this will not work
-                   method(Method),
-                   on_trace(collect_unused(M))
-                  ], Options),
-    option_module_files(Options, MFileD),
-    option_files([module_files(MFileD)], FileD),
-    walk_code([module_files(MFileD)|Options]),
-    option(concurrent(Concurrent), Options, true),
-    mark_compile_time_called,
-    mark(Concurrent),
-    sweep(FileD, Pairs),
+    check_unused(Options, Result),
+    cleanup_calls_to,
     cleanup_unused.
 
-mark_compile_time_called :-
-    forall(distinct(M:H,
-                    ( compile_time_called(H, M, C),
-                      M \= C
-                    )),
-           put_mark('<exported>'(M:H))).
+check_unused(Options, Pairs) :-
+    collect_calls_to(Options, MFileD),
+    option_files([module_files(MFileD)], FileD),
+    option(concurrent(Concurrent), Options, true),
+    mark(Concurrent),
+    sweep(FileD, Pairs).
 
 cleanup_unused :-
-    retractall(calls_to_initialization(_, _)),
-    retractall(calls_to_assertion(_, _, _, _)),
-    retractall(calls_to_declaration(_, _)),
-    retractall(calls_to_clause(_, _, _)),
-    retractall(calls_to_predid(_, _, _, _)),
     retractall(marked_clause(_)),
     retractall(marked_assertion(_, _)),
     retractall(marked_predid(_, _)),
@@ -134,6 +94,7 @@ cleanup_unused :-
     retractall(edge(_, _, _, _, _)).
 
 marked('<assertion>'(M:H)) :- marked_assertion(H, M).
+marked('<public>'(M:H))    :- marked_predid(H, M).
 marked(M:H)                :- marked_predid(H, M).
 marked(clause(Ref))        :- marked_clause(Ref).
 marked('<initialization>') :- marked_initialization.
@@ -141,6 +102,7 @@ marked('<declaration>')    :- marked_declaration.
 marked('<exported>'(M:H))  :- marked_exported(H, M).
 
 record_marked('<assertion>'(M:H)) :- assertz(marked_assertion(H, M)).
+record_marked('<public>'(M:H))    :- assertz(marked_predid(H, M)).
 record_marked(M:H)                :- assertz(marked_predid(H, M)).
 record_marked(clause(Ref))        :- assertz(marked_clause(Ref)).
 record_marked('<initialization>') :- assertz(marked_initialization).
@@ -150,13 +112,17 @@ record_marked('<exported>'(M:H) ) :- assertz(marked_exported(H, M)).
 is_entry_caller('<assertion>'(M:H)) :- entry_caller(M, H).
 is_entry_caller('<initialization>').
 is_entry_caller('<declaration>'   ).
-is_entry_caller(M:H) :- entry_caller(M, H).
+is_entry_caller('<exported>'(_)   ).
+is_entry_caller('<public>'(_)     ).
+is_entry_caller(M:H) :-
+    entry_caller(M, H).
 is_entry_caller(clause(Ref)) :-
     match_head_clause(M:H, Ref), % Unify head
     entry_caller(M, H).
 
 entry_caller(M, H) :-
-    ( is_entry_point(H, M) -> true
+    ( is_entry_point(H, M)
+    ->true
     ; loc_declaration(H, M, goal, _)
     ).
 
@@ -492,65 +458,3 @@ unused_mo_clpfd(clpfd_gcc_num).
 unused_mo_clpfd(clpfd_gcc_vs).
 unused_mo_clpfd(clpfd_gcc_aux).
 unused_mo_clpfd(clpfd_aux).
-
-caller_ptr('<initialization>', _, '<initialization>') :- !.
-caller_ptr('<assertion>'(AH),  _, '<assertion>'(AH) ) :- !.
-caller_ptr('<declaration>',    _, '<declaration>'   ) :- !.
-caller_ptr(_,        clause(Ptr), clause(Ptr)       ) :- !.
-caller_ptr(M:H,                _, M:H).
-
-cu_caller_hook(Caller, Head, CM, Type, Goal, _, From) :-
-    callable(Head),
-    nonvar(CM),
-    predicate_property(CM:Head, implementation_module(M)),
-    ( Type \= lit
-    ->compact_goal(Goal, Comp),
-      record_location_goal(Head, M, Type, CM, Comp, From)
-    ; Caller = '<assertion>'(A:H),
-      member(Goal, [foreign_props:fimport(_),
-                    foreign_props:fimport(_, _),
-                    foreign_props:tgen(_),
-                    foreign_props:sgen(_)])
-    ->( A \= CM
-      ->put_mark('<exported>'(A:H))
-      ; put_mark(A:H)
-      )
-    ; true
-    ),
-    record_calls_to(Type, Caller, Head, M, From),
-    ( M \= CM
-    ->put_mark('<exported>'(M:Head))
-    ; true
-    ).
-
-record_calls_to(Type, Caller, Head, M, From) :-
-    ( memberchk(Type, [use, lit])
-    ->caller_ptr(Caller, From, Ptr),
-      record_calls_to(Ptr, M, Head)
-    ; true
-    ).
-
-calls_to('<initialization>'  ) :- once(calls_to_initialization(_, _)).
-calls_to('<assertion>'(AM:AH)) :- distinct(AH:AM, calls_to_assertion(AH, AM, _, _)).
-calls_to('<declaration>'     ) :- once(calls_to_declaration(_, _)).
-calls_to(clause(Ref)         ) :- distinct(Ref, calls_to_clause(Ref, _, _)).
-calls_to(CM:CH               ) :- distinct(CM:CH, calls_to_predid(CH, CM, _, _)).
-
-calls_to(CRef, M, Head) :- calls_to(CRef, c, M, Head).
-
-calls_to('<initialization>',   _, M, Head) :- calls_to_initialization(   Head, M).
-calls_to('<assertion>'(AM:AH), _, M, Head) :- calls_to_assertion(AH, AM, Head, M).
-calls_to('<declaration>',      _, M, Head) :- calls_to_declaration(      Head, M).
-calls_to(clause(Ref),          _, M, Head) :- calls_to_clause(Ref,       Head, M).
-calls_to(CM:CH,                W, M, Head) :- calls_to_predid(W, CH, CM, Head, M).
-
-calls_to_predid(c, CH, CM, Head, M) :- calls_to_predid(CH, CM,    Head, M).
-calls_to_predid(w, CH, CM, Head, M) :-
-    clause(check_unused:calls_to_predid(CH, CM,    _, _), _, Ref),
-    clause(check_unused:calls_to_predid( _,  _, Head, M), _, Ref).
-
-record_calls_to('<initialization>',   M, Head) :- assertz(calls_to_initialization(   Head, M)).
-record_calls_to('<assertion>'(AM:AH), M, Head) :- assertz(calls_to_assertion(AH, AM, Head, M)).
-record_calls_to('<declaration>',      M, Head) :- assertz(calls_to_declaration(      Head, M)).
-record_calls_to(clause(Ref),          M, Head) :- assertz(calls_to_clause(Ref,       Head, M)).
-record_calls_to(CM:CH,                M, Head) :- assertz(calls_to_predid(CH, CM,    Head, M)).
