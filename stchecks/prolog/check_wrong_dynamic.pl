70986a61b0f60799a439da069fb0c4414f4c3fc5
Edison Mera <mera@process-design-center.com>
2023-01-13 15:22:30 +0100

    Fixed some bugs in cohesive.pl: cm_db/1 was interfeering if several calls to cohesive predicates where made, and they where leaving choice points.  Using freeze/2 again (partially reverted commit 0a8aa5ebf0ec24ea2bdcf2a33df3ef8afa4e078d), since we should avoid leaving green choicepoints

--- stchecks/prolog/check_wrong_dynamic.pl
+++ stchecks/prolog/check_wrong_dynamic.pl
@@ -94,6 +94,7 @@ hide_var_dynamic_hook(goal_expansion(_, _), _).
 hide_var_dynamic_hook(goal_expansion(_, _, _, _), _).
 hide_var_dynamic_hook(raw_load_loop(_, _), raw_loader).
 hide_var_dynamic_hook(ref_head(_, _, _), module_links).
+hide_var_dynamic_hook(check_cohm_clauses(_, _), cohesive).
 
 :- dynamic
     wrong_dynamic_db/4,
