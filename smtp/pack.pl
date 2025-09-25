2cfa7a47fc09dccd0c2d3a0468586ca39b8e8212
Edison Mera <efmera@gmail.com>
2025-09-25 19:51:53 +0200

    smtp is now a submodule

--- smtp/pack.pl
+++
@@ -1,7 +0,0 @@
-name(smtp).
-title('An (E)SMTP client for sending mail').
-version('1.1.0').
-keywords([smtp, mail, sendmail]).
-author('Jan Wielemaker', 'jan@swi-prolog.org').
-home('https://github.com/JanWielemaker/smtp').
-download('https://github.com/JanWielemaker/smtp/releases/*.zip').
