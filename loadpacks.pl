2ebad2c9a00f343be7b003ed7397c72dfe8c285b
Edison Mera <efmera@gmail.com>
2026-05-03 07:57:21 +0200

    Fixed loadpacks to avoid problems when swipl is called from a different directory

--- loadpacks.pl
+++ loadpacks.pl
@@ -1,31 +1,33 @@
 :- use_module(xlibrary/prolog/packloader).
+:- use_module(xlibrary/prolog/call_in_dir).
 :- use_module(library(thread)).
 :- use_module(library(apply)).
-% :- use_module(library(pldoc/doc_htmlsrc)).
 
 :- meta_predicate loadpacks(1).
 
 loadpacks(Loader) :-
     findall(Pack, package(Pack), PackL),
-    scanpacks(
-        PackL,
-        pack_load_local(
-            [ exclude(
-                  [ clpcd/prolog/cd_common,
-                    clpcd/prolog/eval,
-                    idfpml/prolog/bid_auto,
-                    decml/prolog/dec_const,
-                    decml/prolog/dec_pred,
-                    mpfr/prolog/floatn_auto,
-                    mpfr/prolog/floatn_reserve_eps,
-                    xtools/prolog/ws_source,
-                    refactor/prolog/i18n/i18n_trans,
-                    refactor/prolog/ref_expand,
-                    refactor/prolog/ref_replace,
-                    refactor/prolog/ref_replacers,
-                    xlibrary/prolog/compound_expand,
-                    refactor/prolog/ref_scenarios,
-                    stchecks/prolog/stchecks
-                  ])
-            ], Loader),
-        pack_set_local_path).
+    call_in_load_dir(
+        scanpacks(
+            PackL,
+            pack_load_local(
+                [ exclude(
+                      [ clpcd/prolog/cd_common,
+                        clpcd/prolog/eval,
+                        idfpml/prolog/bid_auto,
+                        decml/prolog/dec_const,
+                        decml/prolog/dec_pred,
+                        mpfr/prolog/floatn_auto,
+                        mpfr/prolog/floatn_reserve_eps,
+                        xtools/prolog/ws_source,
+                        refactor/prolog/i18n/i18n_trans,
+                        refactor/prolog/ref_expand,
+                        refactor/prolog/ref_replace,
+                        refactor/prolog/ref_replacers,
+                        xlibrary/prolog/compound_expand,
+                        refactor/prolog/ref_scenarios,
+                        stchecks/prolog/stchecks
+                      ])
+                ], Loader),
+            pack_set_local_path)
+        ).
